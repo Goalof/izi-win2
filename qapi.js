c008686bc8ad20eaa21fbe7d253b49e99d21918c
Goalof <68434460+Goalof@users.noreply.github.com>
2020-09-10 22:38:00 +0600

    Some changes

---
+++ qapi.js
@@ -0,0 +1,54 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f59fc4b0fb50e001fbe8361",
+				"5f59fc4b0fb50e001fbe8363",
+				"5f59fcaf0840a3001ed7e61b",
+				"5f5a2b990fb50e001fbe849e",
+				"5f5a2bbe0840a3001ed7e73c",
+				"5f5a2bc80fb50e001fbe84ac",
+				"5f5a2b99796fc9001efe5450"
+			]
+		},
+		"5f59fc4b0fb50e001fbe8361": {
+			"id": "5f59fc4b0fb50e001fbe8361",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f59fc4b0fb50e001fbe8363": {
+			"id": "5f59fc4b0fb50e001fbe8363",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f59fcaf0840a3001ed7e61b": {
+			"id": "5f59fcaf0840a3001ed7e61b",
+			"pageUrl": "contacts",
+			"name": "CONTACTS"
+		},
+		"5f5a2b990fb50e001fbe849e": {
+			"id": "5f5a2b990fb50e001fbe849e",
+			"pageUrl": "home",
+			"name": "Home"
+		},
+		"5f5a2b99796fc9001efe5450": {
+			"id": "5f5a2b99796fc9001efe5450",
+			"pageUrl": "sontact",
+			"name": "Сontact"
+		},
+		"5f5a2bbe0840a3001ed7e73c": {
+			"id": "5f5a2bbe0840a3001ed7e73c",
+			"pageUrl": "product",
+			"name": "Product"
+		},
+		"5f5a2bc80fb50e001fbe84ac": {
+			"id": "5f5a2bc80fb50e001fbe84ac",
+			"pageUrl": "about",
+			"name": "About"
+		}
+	},
+	"projectType": "gatsby"
+}
